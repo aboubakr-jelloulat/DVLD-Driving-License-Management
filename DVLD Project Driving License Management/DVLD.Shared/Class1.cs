b62d69dac3d4fd3e1e10d2d49268570c2b26c473
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-07-25 12:28:07 +0100

    3-tier Architecture Folder structure

---
+++ DVLD Project Driving License Management/DVLD.Shared/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD.Shared
+{
+    public class Class1
+    {
+    }
+}
